68dec6978d3bb10e228f3c292bf7caaad91a6239
Dev Arora <cpcdevar@gmail.com>
2021-08-24 19:20:56 +0530

    preview page ui completed

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -1,6 +1,22 @@
+import "./Preview.css"
+
 let Preview = ()=>{
     return <>
-    Preview
+    <div className="preview-container">
+        
+        <div className="preview-img">
+            <img src="https://m.media-amazon.com/images/I/61fy+u9uqPL._SL1500_.jpg" alt="some-item" />
+        </div>
+
+        <div className="preview-listing">
+            
+            <h2>Item-Name</h2>
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ullam eius explicabo voluptatibus ratione assumenda deserunt harum ad sit corporis quasi laudantium quam, et eaque asperiores quae soluta, inventore debitis, recusandae blanditiis ab molestiae cupiditate. Vel sed minus facilis ipsa magni provident delectus sapiente laboriosam porro quod necessitatibus fuga iure cum dolorum quo, dolor earum possimus a laudantium, libero commodi consectetur! Animi cupiditate porro a fugit explicabo, nostrum aperiam ea, placeat doloremque reprehenderit molestiae aliquam quo est accusamus impedit eius id sunt temporibus minus sint tenetur repellendus ut? Laudantium dicta esse suscipit adipisci, explicabo aliquid id veniam asperiores! Totam, eos!</p>
+
+            <button>Add to Cart</button>
+        </div>
+    </div>
     </>
 }
 
--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,8 +1,10 @@
 import "./ProductCard.css";
 
 let ProductCard = ()=>{
+
     return(<div className="product-card">
-        <div className="product-img">
+        <div  
+        className="product-img">
             <img src="https://m.media-amazon.com/images/I/61fy+u9uqPL._SL1500_.jpg" alt="some-product" />
         </div>
         <div className="product-btn">
