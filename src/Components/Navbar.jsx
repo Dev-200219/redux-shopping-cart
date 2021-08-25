05dadb867a5eb079aa7de49c1ec80dd0bf70c051
Dev Arora <cpcdevar@gmail.com>
2021-08-25 12:51:56 +0530

    cart btn added to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,14 @@
 import {Link} from "react-router-dom"
+import "./Navbar.css"
+
 let Navbar = () => {
     return <>
         <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Shopping Cart</Link>
+                <Link className="navbar-brand" to="/cart">Cart
+                <span class="material-icons-outlined">shopping_cart</span>
+                </Link>
             </div>
         </nav>
     </>
